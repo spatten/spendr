f03f025fc41bd7bea514119dc0f02a199695ad24
Scott Patten <scott@ruboss.com>
2009-04-17 13:40:22 -0700

    adding Category#expenditure_sum and #expenditure_count

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ActiveRecord::Base
   belongs_to :user
   has_many :expenditures, :dependent => :destroy
+  
+  def expenditure_sum
+    Expenditure.sum(:amount, :conditions => ['category_id = ?', id])
+  end
+  
+  def expenditure_count
+    Expenditure.count(:conditions => ['category_id = ?', id])
+  end
 end
